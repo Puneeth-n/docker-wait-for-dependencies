4184975668e9f5c410e6efff539e791902a2e720
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2017-09-11 18:32:32 +0200

    add timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
 : ${SLEEP_LENGTH:=2}
+: ${TIMEOUT:=120}
 
 wait_for() {
+
+  CURRENT_WAIT_TIME=0
   echo Waiting for $1 to listen on $2...
-  while ! nc -z $1 $2; do echo sleeping; sleep $SLEEP_LENGTH; done
+  while ! nc -z $1 $2; do
+    CURRENT_WAIT_TIME=`expr $CURRENT_WAIT_TIME + $SLEEP_LENGTH`
+
+    if [ ${CURRENT_WAIT_TIME} -lt ${TIMEOUT} ]; then
+        echo sleeping; sleep $SLEEP_LENGTH;
+        continue
+    else
+        echo Service $1 never came up on port: $2...
+        exit 1
+    fi
+  done
 }
 
 for var in "$@"
